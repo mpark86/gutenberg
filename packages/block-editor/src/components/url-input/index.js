ad74085c688f5fcecff72c48a53699894821de24
retrofox <rdsuarez@gmail.com>
2019-10-24 13:10:02 -0300

    url-input: rollback some changes

--- packages/block-editor/src/components/url-input/index.js
+++ packages/block-editor/src/components/url-input/index.js
@@ -179,6 +179,8 @@ class URLInput extends Component {
 			return;
 		}
 
+		const suggestion = this.state.suggestions[ this.state.selectedSuggestion ];
+
 		switch ( event.keyCode ) {
 			case UP: {
 				event.stopPropagation();
@@ -200,7 +202,7 @@ class URLInput extends Component {
 			}
 			case TAB: {
 				if ( this.state.selectedSuggestion !== null ) {
-					this.selectLink( selectedSuggestion );
+					this.selectLink( suggestion );
 					// Announce a link has been selected when tabbing away from the input field.
 					this.props.speak( __( 'Link selected.' ) );
 				}
@@ -209,7 +211,7 @@ class URLInput extends Component {
 			case ENTER: {
 				if ( this.state.selectedSuggestion !== null ) {
 					event.stopPropagation();
-					this.selectLink( selectedSuggestion );
+					this.selectLink( suggestion );
 				}
 				break;
 			}
